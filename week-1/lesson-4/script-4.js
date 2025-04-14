ca593b1638b7953c11bed2a8dfa23aeaf70cd18b
alejandrorumi-dev <alejandrorumi.dev@gmail.com>
2025-04-15 01:19:43 +0200

    Delete content

--- week-1/lesson-4/script-4.js
+++ week-1/lesson-4/script-4.js
@@ -4,9 +4,7 @@
 Muestra en consola los números del 1 al 10 usando un for.
 */
 
-for (let i = 1; i <=10; i++) {
-	console.log(i);
-}
+
 
 /*
 🔢 Ejercicio 2 - suma acumulada
@@ -15,15 +13,7 @@ Usa un for para sumar los números del 1 al 100 y
 guarda el resultado en una variable suma. Luego muestra el resultado.
 */
 
-let suma = 0; // se crea una variable donde suma vale 0
-for (let k = 1; k <= 100; k++) {
-	/*
-	empieza en 1, la condición se da 100 veces, incrementa
-	*/
 
-	suma += k; // x = x + 1
-}
-console.log(suma);
 
 /*
 🧮 Ejercicio 3 - tabla de multiplicar
@@ -31,14 +21,40 @@ console.log(suma);
 Usa un for para mostrar la tabla del 7 (del 1 al 10) en consola.
 */
 
-for (let i = 0; i <= 10; i++) { 
-	/* 
-	empieza en 0, la condición se da 10 veces, incrementa
-	El resultado de este for es: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 y 10
-	*/
-	console.log(7 * i); 
-	/*
-	se multiplica i por 7
-	7*0, 7*1, 7*2, 7*3... 7*10
-	*/
-}
+
+
+/*
+🔁 Ejercicio 4 - while con contador
+
+Crea un bucle while que cuente desde 10 hasta 1 y lo muestre en consola.
+*/
+
+
+
+/*
+➕ Ejercicio 5 - do while validación
+
+Simula que un usuario tiene que ingresar un número mayor que 0. 
+Usa un do...while para seguir pidiéndolo mientras el número 
+ingresado no sea válido. (Puedes simular el número con una variable 
+en lugar de prompt si aún no lo usas).
+*/
+
+
+
+/*
+🧩 Ejercicio 6 - Suma de números pares
+
+Haz un programa que calcule la suma de todos los números 
+pares del 1 al 100 usando un for.
+*/
+
+
+
+/*
+🔁 Ejercicio 7 - Contador hasta número ingresado
+
+Usa un while para contar desde 1 hasta un número 
+ingresado por el usuario mediante prompt.
+*/
+
