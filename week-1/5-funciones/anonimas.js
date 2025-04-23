395b94d7f64cba549cb70f55c96064ba6dc1ee15
alejandrorumi-dev <alejandrorumi.dev@gmail.com>
2025-04-24 00:22:24 +0200

    Add new exercise 'functions'

---
+++ week-1/5-funciones/anonimas.js
@@ -0,0 +1,8 @@
+/*
+En este ejemplo, utilizamos una función anónima 
+dentro de un setTimeout para ejecutar código después de un tiempo:
+*/
+
+setTimeout(function() {
+    console.log("Han pasado 3 segundos.");
+}, 3000);
