d9d0bbef14d090957106e6c5557396c280abbbc1
alejandrorumi-dev <alejandrorumi.dev@gmail.com>
2025-04-28 10:23:55 +0200

    Add new methods

---
+++ week-2/2-metodos/combinacion/concat.js
@@ -0,0 +1,11 @@
+/*
+concat()
+
+➡️ Junta [1, 2, 3] y [4, 5, 6] en uno solo.
+*/
+
+let numerosUno = [1, 2, 3]
+let numerosDos = [4, 5, 6]
+let numerosTres = numerosUno.concat(numerosDos);
+
+console.log(numerosTres);
---
+++ week-2/2-metodos/combinacion/join.js
@@ -0,0 +1,12 @@
+/*
+join()
+
+➡️ En ['sol', 'luna', 'estrella'], únelos en un 
+string separado por espacios.
+*/
+
+let planeta = ['sol', 'luna', 'estrella']
+
+let string = planeta.join(" ");
+
+console.log(string);
