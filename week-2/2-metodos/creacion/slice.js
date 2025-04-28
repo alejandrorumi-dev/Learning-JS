3c43f481eaa66937212e083f564875ed61b17858
alejandrorumi-dev <alejandrorumi.dev@gmail.com>
2025-04-28 10:34:33 +0200

    Add new method

---
+++ week-2/2-metodos/creacion/slice.js
@@ -0,0 +1,12 @@
+/*
+slice()
+
+➡️ De ['a', 'b', 'c', 'd', 'e'], crea una copia 
+desde 'b' hasta 'd' (sin incluir 'e').
+*/
+
+let letras = ['a', 'b', 'c', 'd', 'e']
+
+let letrasCopiadas = letras.slice(1, 4)
+
+console.log(letrasCopiadas);
