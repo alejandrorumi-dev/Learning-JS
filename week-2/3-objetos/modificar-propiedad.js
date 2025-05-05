16319080181f4c5029d6927af2e503b922d896d6
alejandrorumi-dev <alejandrorumi.dev@gmail.com>
2025-05-05 09:47:05 +0200

    Add new exercise: modify property

---
+++ week-2/3-objetos/modificar-propiedad.js
@@ -0,0 +1,25 @@
+/*
+🔹 3. MODIFICAR UNA PROPIEDAD
+
+Enunciado:
+
+Cambia la edad de la persona a 30.
+*/
+
+let persona = {
+	nombre: "Lucía",
+	edad: 28,
+	ciudad: "Madrid"
+};
+
+persona.edad = 30;
+
+/*
+ELIMINAR:
+
+delete persona.ciudad;
+
+AGREGAR:
+
+persona.profesion = "Periodista";
+*/
