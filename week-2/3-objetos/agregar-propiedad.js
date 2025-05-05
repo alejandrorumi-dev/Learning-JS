d219639267a012950f63f5c1bf0c235cc4a1061e
alejandrorumi-dev <alejandrorumi.dev@gmail.com>
2025-05-05 09:49:15 +0200

    Add new exercise: add property

---
+++ week-2/3-objetos/agregar-propiedad.js
@@ -0,0 +1,16 @@
+/*
+🔹 4. AGREGAR UNA NUEVA PROPIEDAD
+
+Enunciado:
+
+Agrega una nueva propiedad llamada profesion con 
+el valor "Diseñadora".
+*/
+
+let persona = {
+	nombre: "Lucía",
+	edad: 28,
+	ciudad: "Madrid"
+};
+
+persona.profesion = "Diseñadora";
