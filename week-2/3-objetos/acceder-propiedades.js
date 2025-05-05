47571b69db5732bad92d998d1e5119f9481c71cc
alejandrorumi-dev <alejandrorumi.dev@gmail.com>
2025-05-05 09:42:24 +0200

    Add new exercise: access properties

---
+++ week-2/3-objetos/acceder-propiedades.js
@@ -0,0 +1,18 @@
+/*
+🔹 2. ACCEDER A PROPIEDADES
+Enunciado:
+Muestra por consola el nombre y la edad de la persona usando:
+
+notación punto
+
+notación con corchetes
+*/
+
+let persona = {
+	nombre: "Lucía",
+	edad: 28,
+	ciudad: "Madrid"
+};
+
+console.log(persona.nombre)
+console.log(persona["edad"]) // console.log(persona.edad);
