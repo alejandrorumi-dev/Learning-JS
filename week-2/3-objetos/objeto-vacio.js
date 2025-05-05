5d1a3ad5689482b3526d76ca9ea9d53664f2c31e
alejandrorumi-dev <alejandrorumi.dev@gmail.com>
2025-05-05 09:57:26 +0200

    Add new exercise: create empty object

---
+++ week-2/3-objetos/objeto-vacio.js
@@ -0,0 +1,19 @@
+/*
+🔹 6. CREAR OBJETO VACÍO Y AÑADIR DATOS
+
+Enunciado:
+
+Crea un objeto vacío llamado libro, y luego agrega 
+estas propiedades:
+
+titulo: "Aprende JavaScript"
+
+paginas: 350
+*/
+
+let libro = {
+
+};
+
+libro.titulo = "Aprende JavaScript";
+libro.paginas = 350;
