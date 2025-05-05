61fc1be5b1aa01a834596e7fb031657123aacf5e
alejandrorumi-dev <alejandrorumi.dev@gmail.com>
2025-05-05 09:35:32 +0200

    Add new exercise: create a object

---
+++ week-2/3-objetos/crear-objetos.js
@@ -0,0 +1,25 @@
+/*
+🔹 1. CREAR OBJETO
+Enunciado:
+Crea un objeto llamado persona que tenga las 
+siguientes propiedades:
+
+nombre: "Lucía"
+
+edad: 28
+
+ciudad: "Madrid"
+*/
+
+let persona = {
+	nombre: "Lucía",
+	edad: 28,
+	ciudad: "Madrid"
+};
+
+/*
+Explicación con mis palabras:
+
+Se crea una variable, en este caso llamada "persona", colocamos
+"{}" y dentro creamos los atributos relacionados con "persona"
+*/
