5cfd0a1fb21a057b1333ab844e911c9c35365912
alejandrorumi-dev <alejandrorumi.dev@gmail.com>
2025-05-05 09:51:55 +0200

    Add new exercise: delete property

---
+++ week-2/3-objetos/eliminar-propiedad.js
@@ -0,0 +1,15 @@
+/*
+🔹 5. ELIMINAR UNA PROPIEDAD
+
+Enunciado:
+
+Elimina la propiedad ciudad del objeto persona.
+*/
+
+let persona = {
+	nombre: "Lucía",
+	edad: 28,
+	ciudad: "Madrid"
+};
+
+delete persona.ciudad;
